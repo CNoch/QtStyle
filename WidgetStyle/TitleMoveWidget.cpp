305f34313ebe2aac1891c159318ebc9bfcbe4470
CNoch <wr389110442@outlook.com>
2020-06-09 10:02:00 +0800

    窗口标题栏简单定制

--- WidgetStyle/TitleMoveWidget.cpp
+++ WidgetStyle/TitleMoveWidget.cpp
@@ -1,6 +1,35 @@
 #include "TitleMoveWidget.h"
+#include <QPainter>
+#include <QStyleOption>
 
-TitleMoveWidget::TitleMoveWidget(QWidget *parent) : QWidget(parent)
+TitleMoveWidget::TitleMoveWidget(int height, const QString &style, QWidget *parent) : QWidget(parent)
 {
+    setFixedSize(parent->width(), height);
+    move(parent->x(),parent->y());
+    setStyleSheet(style);
+}
 
+void TitleMoveWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        QWidget *parentWidget = this->parentWidget();
+        QPoint point = parentWidget->pos();
+        point.setX(point.x() + event->x() - m_PressPoint.x());
+        point.setY(point.x() + event->y() - m_PressPoint.y());
+        parentWidget->move(point);
+    }
+}
+
+void TitleMoveWidget::mousePressEvent(QMouseEvent *event)
+{
+    m_PressPoint = event->pos();
+}
+
+void TitleMoveWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter p(this);
+    QStyleOption opt;
+    opt.init(this);
+    style()->drawPrimitive(QStyle::PE_Widget,&opt,&p,this);
 }
--- WidgetStyle/TitleMoveWidget.h
+++ WidgetStyle/TitleMoveWidget.h
@@ -1,16 +1,24 @@
 #ifndef TITLEMOVEWIDGET_H
 #define TITLEMOVEWIDGET_H
+//提供移动窗口的标题栏 2020/06/09 10:00:00
 
 #include <QWidget>
+#include <QMouseEvent>
+#include <QPaintEvent>
 
 class TitleMoveWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit TitleMoveWidget(QWidget *parent = nullptr);
+    explicit TitleMoveWidget(int height = 30, const QString &style = nullptr, QWidget *parent = nullptr);
 
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 signals:
 
+private:
+    QPoint m_PressPoint;
 };
 
 #endif // TITLEMOVEWIDGET_H
